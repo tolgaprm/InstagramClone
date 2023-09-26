1a042e92873bfa0de29b8ba671073f0b401cd2fc
Tolga Pirim <tolgapirim25@gmail.com>
2023-09-26 13:59:05 +0300

    Insta-1009 Users can update the profile image #13
    + Upgrade the gradle to 8.0.2
    + Solve error with /.gradlew build

--- build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
@@ -3,6 +3,7 @@ import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
+import org.jetbrains.kotlin.gradle.dsl.kotlinExtension
 
 class AndroidLibraryConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -25,6 +26,8 @@ class AndroidLibraryConventionPlugin : Plugin<Project> {
                         sourceCompatibility = JavaVersion.VERSION_17
                         targetCompatibility = JavaVersion.VERSION_17
                     }
+
+                    kotlinExtension.jvmToolchain(17)
                 }
             }
         }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,10 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
+}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
